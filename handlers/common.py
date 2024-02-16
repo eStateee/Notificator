c1539aba9da80f7ce10d198c2d2d4e4b0f26e0c8
sasha <helen_kirillova@mail.ru>
2024-02-16 10:10:16 +0300

    README and text info update

--- handlers/common.py
+++ handlers/common.py
@@ -2,16 +2,22 @@ from aiogram.dispatcher import FSMContext
 from aiogram.types import Message, ReplyKeyboardRemove
 from keyboards.common_keybaords import get_register_inline_keyboard
 from services.user_service import get_user_by_id
+from config import COMMANDS
+from keyboards.common_keybaords import get_main_keyboard
 
 
 async def start(message: Message, state: FSMContext, session):
     await state.finish()
     user = await get_user_by_id(user_id=message.from_user.id, session=session)
     if not user:
-        await message.answer(f'Привет, что бы пользоваться ботом тебе необходимо пройти регистрацию',
+        await message.answer(f'Привет:)\nЭтот бот служит для напоминания тебе твоих важных и не очень дел.\n '
+                             f'Что бы начать пользоваться ботом тебе необходимо пройти регистрацию',
                              reply_markup=get_register_inline_keyboard())
     else:
-        await message.answer('Привет:)')
+        cmds = ''
+        for i in COMMANDS:
+            cmds += f'/{i[0]} -- {i[1]}\n'
+        await message.answer(f'И снова здравствуй!\nСписок команд:\n{cmds}', reply_markup=get_main_keyboard())
 
 
 async def cancel(message: Message, state: FSMContext):
